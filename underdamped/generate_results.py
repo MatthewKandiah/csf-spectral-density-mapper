958ddf0aafff43274bdd98a5a1cb9254e3d693e8
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-08-23 20:49:39 +0100

    fixed relative path, added reminder comment for what to do next time

--- underdamped/generate_results.py
+++ underdamped/generate_results.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import sys
 
 '''
 pull number of plot points from command line variables
@@ -8,7 +9,8 @@ pull number of plot points from command line variables
 if __name__ == "__main__":
 	NUMBER_OF_PLOT_POINTS = int(sys.argv[1])
 
-plot_points = np.array()
+plot_points = np.array() 
+### np.array can't be called with zero parameters. Maybe better to do a list, convert to np.array, then generate csv
 
 for i in range(0,NUMBER_OF_PLOT_POINTS):
 	in_file = open(f"/picklejar/plot_point_{i}.pickle", rb)
