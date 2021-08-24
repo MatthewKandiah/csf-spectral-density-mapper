2351b52784d8ecdfc6d782dc07e833ad3731a640
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-08-24 19:39:15 +0100

    trying different sort method, hopefully this one doesn't error

--- underdamped/generate_results.py
+++ underdamped/generate_results.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
 plot_points = np.zeros((NUMBER_OF_PLOT_POINTS, 2))
 
 for i in range(0,NUMBER_OF_PLOT_POINTS):
-	in_file = open(f"./picklejar/plot_point_{i}.pickle", rb)
+	in_file = open(f"./picklejar/plot_point_{i}.pickle", 'rb')
 	plot_point = pickle.load(in_file)
 	plot_points[i][0] = plot_point[0]
 	plot_points[i][1] = plot_point[1]
 	in_file.close()
 
-plot_points.sort(key = lambda x: x[0])
+plot_points = plot_points[plot_points[:,0].argsort()]
 
 plot_points.tofile('./results/mapped_underdamped_spectral_density.csv', sep = ',')
 
