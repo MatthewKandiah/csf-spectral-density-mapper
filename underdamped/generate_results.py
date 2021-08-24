19a67c5ca60b1c30143baebf21187ac4af283ec2
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-08-24 19:55:14 +0100

    store results as 2 rows instead of 2 columns

--- underdamped/generate_results.py
+++ underdamped/generate_results.py
@@ -8,17 +8,18 @@ import sys
 if __name__ == "__main__":
 	NUMBER_OF_PLOT_POINTS = int(sys.argv[1])
 
-plot_points = np.zeros((NUMBER_OF_PLOT_POINTS, 2))
+plot_points = np.zeros((2, NUMBER_OF_PLOT_POINTS))
 
 for i in range(0,NUMBER_OF_PLOT_POINTS):
 	in_file = open(f"./picklejar/plot_point_{i}.pickle", 'rb')
 	plot_point = pickle.load(in_file)
-	plot_points[i][0] = plot_point[0]
-	plot_points[i][1] = plot_point[1]
+	plot_points[0][i] = plot_point[0]
+	plot_points[1][i] = plot_point[1]
 	in_file.close()
 
-plot_points = plot_points[plot_points[:,0].argsort()]
+plot_points = plot_points[::, plot_points[0,].argsort()]
 
 plot_points.tofile('./results/mapped_underdamped_spectral_density.csv', sep = ',')
 
+plt.plot(plot_points)
 plt.savefig('./results/mapped_underdamped_spectral_density.png')
