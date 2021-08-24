219805c24a11c5c5a3d4f45e1c19e76d944d4853
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-08-24 20:22:25 +0100

    generate results in 2 columns for easy to read csv, transform to 2 rows just for plotting

--- underdamped/generate_results.py
+++ underdamped/generate_results.py
@@ -2,24 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import sys
+import csv
 
 ### pull number of plot points from command line variables
 
 if __name__ == "__main__":
 	NUMBER_OF_PLOT_POINTS = int(sys.argv[1])
 
-plot_points = np.zeros((2, NUMBER_OF_PLOT_POINTS))
+plot_points = []
 
 for i in range(0,NUMBER_OF_PLOT_POINTS):
 	in_file = open(f"./picklejar/plot_point_{i}.pickle", 'rb')
 	plot_point = pickle.load(in_file)
-	plot_points[0][i] = plot_point[0]
-	plot_points[1][i] = plot_point[1]
+	plot_points.append(plot_point)
 	in_file.close()
 
-plot_points = plot_points[::, plot_points[0,].argsort()]
+### plot results
 
-plot_points.tofile('./results/mapped_underdamped_spectral_density.csv', sep = ',')
-
-plt.plot(plot_points)
+plt.plot([x[0] for x in plot_points],[y[0] for y in plot_points])
+plt.xlabel('Frequency')
+plt.ylabel('Mapped spectral density')
 plt.savefig('./results/mapped_underdamped_spectral_density.png')
+
+### write results to csv
+
+with open("./results/mapped_underdamped_spectral_density.csv", "w") as out_file:
+	writer = csv.writer(out_file)
+	writer.writerows()
