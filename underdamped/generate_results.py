44b72ea50690a6c420324fa40011444df41fece8
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-08-24 19:18:10 +0100

    avoid using empty numpy arrays

--- underdamped/generate_results.py
+++ underdamped/generate_results.py
@@ -3,23 +3,22 @@ import numpy as np
 import pickle
 import sys
 
-'''
-pull number of plot points from command line variables
-'''
+### pull number of plot points from command line variables
+
 if __name__ == "__main__":
 	NUMBER_OF_PLOT_POINTS = int(sys.argv[1])
 
-plot_points = np.array() 
-### np.array can't be called with zero parameters. Maybe better to do a list, convert to np.array, then generate csv
+plot_points = np.zeros((NUMBER_OF_PLOT_POINTS, 2))
 
 for i in range(0,NUMBER_OF_PLOT_POINTS):
-	in_file = open(f"/picklejar/plot_point_{i}.pickle", rb)
+	in_file = open(f"./picklejar/plot_point_{i}.pickle", rb)
 	plot_point = pickle.load(in_file)
-	np.append(plot_points, plot_point)
+	plot_points[i][0] = plot_point[0]
+	plot_points[i][1] = plot_point[1]
 	in_file.close()
 
 plot_points.sort(key = lambda x: x[0])
 
-plot_points.tofile('results/mapped_underdamped_spectral_density.csv', sep = ',')
+plot_points.tofile('./results/mapped_underdamped_spectral_density.csv', sep = ',')
 
-plt.savefig('results/mapped_underdamped_spectral_density.png')
+plt.savefig('./results/mapped_underdamped_spectral_density.png')
