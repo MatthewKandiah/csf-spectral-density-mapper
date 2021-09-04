d266c00f662caee7143a6c0f95cd31be14b72bc3
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-09-04 17:44:27 +0100

    fixed gitignore and typos in plot results script

--- underdamped/plot_results.py
+++ underdamped/plot_results.py
@@ -3,21 +3,17 @@ import numpy as np
 import pandas as pd
 import csv
 
-### run this in the same directory as your csv containing your plot points
+### run this in the underdamped directory, in the place where this file is saved
+### otherwise the paths won't be defined correctly
 
 plot_points_frame = pd.read_csv('./results/mapped_underdamped_spectral_density.csv')
 plot_points_array = plot_points_frame.values
 frequencies = plot_points_array[:,0]
 rc_frequencies = plot_points_array[:,1]
 
-print(frequencies)
-print(rc_frequencies)
-
-plt.title('Mappeing an Underdamped Spectral Density')
-plt.xlabel('frequency')
-plt.ylabel('mapped spectral density')
-# plt.xticks(np.arange(0, 1, step=0.2))
-# plt.yticks(np.arange(0,0.16, step=0.02))
-plt.axis([0,1,0,0.16])
+plt.title('Mapping an Underdamped Spectral Density')
+plt.xlabel('Frequency')
+plt.ylabel('Mapped Spectral Density')
+plt.axis([ 0, 1.1, 0, 0.17 ])
 plt.plot(frequencies,rc_frequencies)
-plt.savefig('test.png')
+plt.savefig('./results/mapped_underdamped_spectral_density.png')
