a1a7f390e57807c6e0ddb98dfba3c11a7d931c92
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-09-06 20:25:57 +0100

    plot results

--- overdamped/slow/plot_results.py
+++ overdamped/slow/plot_results.py
@@ -13,3 +13,35 @@ first_moments = plot_points_array[:,1]
 reorganisation_energies = plot_points_array[:,2]
 rc_frequencies = plot_points_array[:,3]
 rc_system_coupling_strengths = plot_points_array[:,4]
+
+### plot cutoff against first-moment
+plt.title('Cutoff vs First Moment')
+plt.xlabel('Cutoff Frequency')
+plt.ylabel('First Moment of Unmapped Spectral Density')
+plt.plot(cutoff_frequencies,first_moments)
+plt.savefig('./results/cutoff_vs_first_moment.png')
+plt.clf()
+
+### plot cutoff against reorganisation energy
+plt.title('Cutoff vs Reorganisation Energy')
+plt.xlabel('Cutoff Frequency')
+plt.ylabel('Reorganisation Energy')
+plt.plot(cutoff_frequencies,reorganisation_energies)
+plt.savefig('./results/cutoff_vs_reorganisation_energy.png')
+plt.clf()
+
+### plot cutoff against rc frequency
+plt.title('Cutoff vs RC Frequency')
+plt.xlabel('Cutoff Frequency')
+plt.ylabel('RC Frequency')
+plt.plot(cutoff_frequencies,rc_frequencies)
+plt.savefig('./results/cutoff_vs_rc_frequency.png')
+plt.clf()
+
+### plot cutoff against rc-system coupling strength
+plt.title('Cutoff vs RC Coupling Strength')
+plt.xlabel('Cutoff Frequency')
+plt.ylabel('RC Coupling Strength')
+plt.plot(cutoff_frequencies,rc_system_coupling_strengths)
+plt.savefig('./results/cutoff_vs_rc_coupling_strength.png')
+plt.clf()
