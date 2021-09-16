d44cfdc41eec89baaf42ee692c5ca4ec9ddc83ea
Matthew Kandiah <mj.kandiah@googlemail.com>
2021-09-16 08:09:15 +0100

    added steady_state_solver function

--- master_equation_solver.py
+++ master_equation_solver.py
@@ -6,6 +6,7 @@ class Bath:
 		self.spectral_density = spectral_density
 		self.temperature = temperature
 
+
 class State(qt.Qobj):
 	def check_valid(self, tolerance=0):
 		if self.shape[0] != self.shape[1]:
@@ -14,3 +15,14 @@ class State(qt.Qobj):
 			raise ValueError(f"State expects Hermitian matrix. Received {self.data}")
 		if abs(self.tr() - 1) > tolerance:
 			raise ValueError(f"State expected to be normalised up to errors of the order {tolerance}. Received matrix with trace = {self.tr()}")
+
+
+def steady_state_solver(Liouvillian, solver_method, absolute_tolerance):
+	rhoSS_steady = qt.steadystate(
+		liouvillian, 
+		method=solver_method, 
+		sparse=True, 
+		tol = absolute_tolerance,
+		maxiter = 100000,
+		use_precond = True)
+    return rhoSS_steady
